642d12c45bf706b0978b2d632ccfa8dac1fcf3dd
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-05-09 16:14:21 +0800

    Update info.js

--- modules/commands/info.js
+++ modules/commands/info.js
@@ -8,7 +8,7 @@ module.exports.config = {
 		version: "1.0.1",
 		aliases: ["info", "owner", "Owner", "Info", "in", "fo"],
 		role: 0,
-		credits: "cliff",
+		credits: "ericson",
 		description: "Admin and Bot info.",
 		cooldown: 5,
 	commandCategory: "info",
@@ -28,7 +28,7 @@ module.exports.run = async function({ api, event, _GLOBAL, args, prefix, admin }
 		let seconds = Math.floor(time % 60);
 		const uptimeString = `${years > 0 ? `${years} years ` : ''}${months > 0 ? `${months} months ` : ''}${weeks > 0 ? `${weeks} weeks ` : ''}${days % 7 > 0 ? `${days % 7} days ` : ''}${hours > 0 ? `${hours} hours ` : ''}${minutes > 0 ? `${minutes} minutes ` : ''}${seconds} seconds`;
 
-		const FILESOWNER = "<𝗖𝗶𝗱>";
+		const FILESOWNER = "<ericson>";
 		const juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 		const link = ["https://i.imgur.com/9LDVC57.mp4", "https://i.imgur.com/r7IxgiR.mp4", "https://i.imgur.com/J1jWubu.mp4", "https://i.imgur.com/DJylTiy.mp4", "https://i.imgur.com/v4mLGte.mp4", "https://i.imgur.com/uthREbe.mp4", "https://i.imgur.com/ee8fHna.mp4", "https://i.imgur.com/VffzOwS.mp4", "https://i.imgur.com/ci5nztg.mp4", "https://i.imgur.com/qHPeKDV.mp4", "https://i.imgur.com/Rkl5UmH.mp4",
 "https://i.imgur.com/IGXINCB.mp4",
